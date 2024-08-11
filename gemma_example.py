d1a49ae58bd4c4cdce370bda17b0d813d60c8061
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-08-11 10:13:09 +0200

    exportable bf16

--- gemma_example.py
+++ gemma_example.py
@@ -9,20 +9,24 @@ from functools import partial
 import json
 from transformers.models.gemma.modeling_gemma import GemmaAttention
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 original_gemma_forward = GemmaAttention.forward
 self_extend_forward = partial(
-    GemmaSE.self_extend_forward, group_size_1=8, group_size_2=1024
+    GemmaSE.self_extend_forward, group_size_1=16, group_size_2=2048
 )
 
 device = "cpu"
-model_path = "google/gemma-2b-it"
-model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
+model_path = "google/gemma-2-2b-it"
+model = AutoModelForCausalLM.from_pretrained(
+  model_path,
+  torch_dtype=torch.bfloat16,
+).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model.eval()
 
 
-for line in open("passkey_examples_5k.jsonl", "r"):
+for line in open("passkey_examples_10k.jsonl", "r"):
     example = json.loads(line)
     prompt_postfix = "What is the pass key? The pass key is "
     prompt = example["input"] + prompt_postfix
@@ -59,3 +63,6 @@ for line in open("passkey_examples_5k.jsonl", "r"):
     answer = answer.replace("\n", "\\n")
     print(answer)
     print("-----------------------------------\n")
+
+modify_method_of_instance(model, "GemmaAttention", "forward", self_extend_forward)
+model.save_pretrained("SelfExtend")
